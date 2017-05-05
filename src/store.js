f7bf3265424b1ea4df825fa0f30ae7c2c5b46e33
ugility <ugility@gmail.com>
2017-05-05 15:44:57 -0400

    Updated package to include additional dependencies: * rxjs * gsap * redux-thunk

--- src/store.js
+++ src/store.js
@@ -2,9 +2,10 @@ import { applyMiddleware, createStore } from 'redux';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux';
 import reducers from './reducers/Index';
+import thunk from 'redux-thunk';
 
 // import {createLogger} from 'redux-logger';
-// import thunk from 'redux-thunk';
+
 
 export const history = createBrowserHistory();
 
@@ -17,7 +18,7 @@ export const history = createBrowserHistory();
     }
 };
  **/
-const middleware = applyMiddleware(routerMiddleware(history));
+const middleware = applyMiddleware(thunk, routerMiddleware(history));
 
 // const middleware = applyMiddleware(thunk, error, routerMiddleware(history));
 // const middleware = applyMiddleware(thunk, createLogger(), error, routerMiddleware(history));
